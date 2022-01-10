4c19cb0087d2becb5fada83c862da0df86fec882
NicolasPerez35 <44806757+NicolasPerez35@users.noreply.github.com>
2022-01-10 13:01:13 -0300

    add { getRoles in role.js }

--- bot/utils/role.js
+++ bot/utils/role.js
@@ -9,6 +9,15 @@ const getPermissions = (permissions) => {
     return permissionString;
 }
 
+const getRoles = (roles) => {
+    let message = 'Has no roles. (Except for the @everyone role 😉)';
+    const rolesToReturn = roles.filter(r => r.name !== '@everyone');
+    if (rolesToReturn.size > 0) message = rolesToReturn.map(r => `<@&${r.id}>`).join(' | ');
+
+    return { text: message, size: rolesToReturn.size };
+}
+
 module.exports = {
     getPermissions,
+    getRoles,
 }
