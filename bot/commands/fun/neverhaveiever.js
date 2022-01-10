9a50238ef54a36ece1f8297fae99ec1d0bcae968
NicolasPerez35 <44806757+NicolasPerez35@users.noreply.github.com>
2022-01-10 15:58:03 -0300

    add { never have i ever command }

---
+++ bot/commands/fun/neverhaveiever.js
@@ -0,0 +1,20 @@
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const fs = require("fs");
+const { guilds_list } = require("../../utils/guilds");
+module.exports = {
+    name: "neverhaveiever",
+    data: new SlashCommandBuilder()
+        .setName("neverhaveiever")
+        .setDescription("Replies with a never have i ever question"),
+    async execute(interaction) {
+        try {
+            const neverHaveIEverQuestions = require(`../../locale/${process.env.LOCALE}/never_have_i_ever.json`);
+            const randomQuestion = neverHaveIEverQuestions.questions[Math.floor(Math.random() * neverHaveIEverQuestions.questions.length)].question;
+
+            interaction.reply(`${neverHaveIEverQuestions.name} ${randomQuestion}`);
+        } catch (error) {
+
+        }
+    }
+
+}
