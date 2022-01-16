55c63e5ddd080e3ad941f60859de76ccb436df2c
NicolasPerez35 <44806757+NicolasPerez35@users.noreply.github.com>
2022-01-16 19:46:18 -0300

    finish { footer of page }

--- web/src/components/Footer/Footer.js
+++ web/src/components/Footer/Footer.js
@@ -4,30 +4,105 @@ import Select from "../Select/SelectLogo";
 
 export default function Footer() {
     return (
-        <footer className="max-w-7xl mx-auto px-4 sm:px-6">
+        <footer className="border-slate-600 border-t-2" >
+            <section className="max-w-7xl mx-auto px-4 sm:px-6 flex justify-between">
+                <section className="py-4 mr-10">
 
-            <section className="w-56 py-4">
+                    <h2 className="text-gray-600 text-lg font-semibold">Features</h2>
 
-                <div className="flex items-center ">
-                    <img
-                        className="h-8 w-auto sm:h-10"
-                        src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
-                    />
-                    <span className="text-base ml-4 font-medium">Obelisk</span>
-                </div>
+                    <ul className="mt-4">
+                        <li className="mt-2">
+                            <a href="#" className="hover:text-gray-900">
+                                <span className="text-gray-600">
+                                    Moderation & Security
+                                </span>
+                            </a>
+                        </li>
+                        <li className="mt-2">
+                            <a href="#" className="hover:text-gray-900">
+                                <span className="text-gray-600">
+                                    Economy simulation
+                                </span>
+                            </a>
+                        </li>
+                        <li className="mt-2">
+                            <a href="#" className="hover:text-gray-900">
+                                <span className="text-gray-600">
+                                    Integrations
+                                </span>
+                            </a>
+                        </li>
+                        <li className="mt-2">
+                            <a href="#" className="hover:text-gray-900">
+                                <span className="text-gray-600">
+                                    Automations
+                                </span>
+                            </a>
+                        </li>
+                    </ul>
+                </section>
 
-                <div className="mt-10">
-                    <span className="text-sm text-gray-500 text-justify">
-                        One of the best discord bots to boost your server to the top.
-                    </span>
+                <section className="py-4 mr-10">
+                    <h2 className="text-gray-600 text-lg font-semibold">Support</h2>
 
-                    <Select 
-                        title={'Language'}
-                        list={Locale}
-                    />
+                    <ul className="mt-4">
+                        <li className="mt-2">
+                            <a href="#" className="hover:text-gray-900">
+                                <span className="text-gray-600">
+                                    Pricing
+                                </span>
+                            </a>
+                        </li>
+                        <li className="mt-2">
+                            <a href="#" className="hover:text-gray-900">
+                                <span className="text-gray-600">
+                                    Documentation
+                                </span>
+                            </a>
+                        </li>
+                        <li className="mt-2">
+                            <a href="#" className="hover:text-gray-900">
+                                <span className="text-gray-600">
+                                    Guides
+                                </span>
+                            </a>
+                        </li>
+                        <li className="mt-2">
+                            <a href="#" className="hover:text-gray-900">
+                                <span className="text-gray-600">
+                                    API Status
+                                </span>
+                            </a>
+                        </li>
+                    </ul>
+                </section>
 
-                </div>
 
+                <section className="w-56 py-4">
+
+                    <div className="flex items-center ">
+                        <img
+                            className="h-8 w-auto sm:h-10"
+                            src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
+                        />
+                        <span className="text-base ml-4 font-medium">Obelisk</span>
+                    </div>
+
+                    <div className="mt-10">
+                        <span className="text-sm text-gray-500 text-justify">
+                            One of the best discord bots to boost your server to the top.
+                        </span>
+
+                        <div className="mt-4">
+                            <Select
+                                title={'Language'}
+                                list={Locale}
+                            />
+                        </div>
+
+                    </div>
+
+                </section>
             </section>
 
         </footer>
