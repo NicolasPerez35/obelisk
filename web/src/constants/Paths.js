a5454f0ba21c4cedcdbcb1b8cebe6f5a4b4295fc
NicolasPerez35 <44806757+NicolasPerez35@users.noreply.github.com>
2022-01-23 16:11:43 -0300

    feat(Paths): Added new paths

--- web/src/constants/Paths.js
+++ web/src/constants/Paths.js
@@ -2,6 +2,12 @@
 export const INDEX = '/';
 export const HOME = '/home';
 export const ABOUT_US = '/about-us';
+export const PRICING = '/pricing';
 export const DOCUMENTATION = '/documentation';
 export const CONTACT_US = '/contact-us';
 //#endregion HEADER PATHS
+
+//#region MISC PATHS
+export const NOT_FOUND = '*';
+export const ADD_PAGE_REQUEST = '/add-page-request';
+//#endregion MISC PATHS
